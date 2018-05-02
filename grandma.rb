f0a3d8cb8a0ffb747cde7c176fda63dccb515597
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-02 13:44:35 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,9 @@
 def speak_to_grandma(phrase)
-
-  if phrase == phrase.upcase()
+phrase == "I LOVE YOU GRANDMA!"
+return "I LOVE YOU TOO PUMPKIN!"
+  if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
-  elsif (phrase == "I LOVE YOU GRANDMA!")
-      return "I LOVE YOU TOO PUMPKIN!"
-  else phrase == phrase.downcase()
+  else 
     return "NO, NOT SINCE 1938!"
 
   end
