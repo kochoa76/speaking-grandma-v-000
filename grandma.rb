b0fa0838a51386978c5d317343d0dca253174636
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-02 13:45:49 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 def speak_to_grandma(phrase)
-phrase == "I LOVE YOU GRANDMA!"
+if phrase == "I LOVE YOU GRANDMA!"
 return "I LOVE YOU TOO PUMPKIN!"
-  if phrase != phrase.upcase()
+end
+
+if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
   else 
     return "NO, NOT SINCE 1938!"
